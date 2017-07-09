ebd3c8531738d2234ec7bbe0bf201c2768a2e030
Allen <1693065785@qq.com>
2017-07-09 19:11:48 +0800

    Text的UI
    生命值的UI

---
+++ Playercontrol.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playercontrol : MonoBehaviour {
+    public float MoveSpeed = 10.0f;
+    private textset blood;
+	// Use this for initialization
+	void Start () {
+        blood = GameObject.Find("Canvas/Text").GetComponent<textset>();
+	}
+	
+
+    
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.left * MoveSpeed * Time.deltaTime);
+        }
+        else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right * MoveSpeed * Time.deltaTime);
+        }
+        blood.a = 1;
+	}
+}
---
+++ textset.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class textset : MonoBehaviour {
+
+    public Text Blood, GameOver;
+    public int a = 0; 
+    private int num = 100;
+
+    // Use this for initialization
+
+    void Start () {
+        Blood = GameObject.Find("Canvas/Text").GetComponent<Text>();
+        GameOver = GameObject.Find("Canvas/Text").GetComponent<Text>();
+	}
+	
+    void Show()
+    {
+        num -= a;
+        if(num <= 0)
+        {
+            GameOver.text = "GameOver";
+        } else
+        {
+            Blood.text = "HP : " + num;
+
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+        Show();
+	}
+}
